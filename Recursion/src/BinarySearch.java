057f7a180614233a1340f69cc95739ed588f20a7
Sarangdeep Singh <sarangdeep.singh93@gmail.com>
2017-11-14 23:58:41 -0500

    Recursion Practice

--- Recursion/src/BinarySearch.java
+++ Recursion/src/BinarySearch.java
@@ -2,20 +2,17 @@
 public class BinarySearch {
 	int BSearch(int[] arr, int start, int end, int key) {
 		int mid=end+ (start-end)/2;	
-		
-		System.out.println(mid);
 		if(arr[mid]==key)
 			return mid;
 		else if(arr[mid]<key )
 			return BSearch(arr, mid+1, end, key);
 		else
-			return BSearch(arr,mid-1, start, key);
+			return BSearch(arr, start,mid-1, key);
 	}
 	public static void main(String[] args) {
 		int[] arr= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
 		int start=0;
-		int end=arr.length;
-		
+		int end=arr.length;		
 		int key= 15;
 		BinarySearch search= new BinarySearch();
 		System.out.println(search.BSearch(arr, start, end, key));
